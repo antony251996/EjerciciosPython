d872efb9ef80c72d95740abeba6aafe60b860d7d
Antony Carrasco <antonycarrasco24@gmail.com>
2020-12-11 17:46:00 -0500

    Ejercicio 1 resuelto de bucles

---
+++ Bucles/Ejercicio1.py
@@ -0,0 +1,6 @@
+#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
+
+palabra = input('Ingresa una palabra: ')
+
+for i in range(10):
+    print(f'La palabra es {palabra}')
